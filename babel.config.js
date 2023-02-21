0812aea00d471af79296334d9d0f3a58cd5d6b94
전홍찬 <chanymac@jeonhongchan-ui-MacBookPro.local>
2023-02-21 23:10:16 +0900

    fix: add assets and fix config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+          '@assets': './src/assets',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@style': './src/style',
+        },
+      },
+    ],
+  ],
 };
